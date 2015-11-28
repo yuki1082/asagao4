ec0b62d30c95a2e2e39f21fd314da90854cf5ca3
negishi <yuki071053@gmail.com>
2015-11-28 15:53:32 +0900

    finish chapter3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,6 @@ module ApplicationHelper
 	end 
 
 	def menu_link_to(text, path)
-		link_to_unless_current(text, path){ conteng_tag(:span, text) }
+		link_to_unless_current(text, path){ content_tag(:span, text) }
 	end 
 end
