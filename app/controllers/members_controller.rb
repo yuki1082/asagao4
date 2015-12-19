26ee908d7fdfea28a59f6c16d1a1bcb65e90f457
negishi <yuki071053@gmail.com>
2015-12-19 20:32:17 +0900

    finish chapter 6

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,31 +1,50 @@
+# encoding: utf-8
+
 class MembersController < ApplicationController
   def index
-  	@members = Member.order(:number)
+    @members = Member.order(:number)
   end
 
   def show
-  	@member = Member.find(params[:id])
+    @member = Member.find(params[:id])
   end
 
   def new
+    @member = Member.new(birthday: Date.new(1980, 1, 1))
   end
 
 
   def create
+    @member = Member.new(params[:member])
+    if @member.save
+      redirect_to @member, notice: "登録しました"
+    else
+      render :new
+    end
   end
 
-
   def edit
+    @member = Member.find(params[:id])
   end
 
   def update
-  end 
+  	@member = Member.find(params[:id])
+  	@member.assign_attributes(params[:member])
+  	if @member.save
+  		redirect_to @member, notice: "更新しました"
+  	else
+  		render :edit
+  	end 
+  end
 
-  def destory
+  def destroy
+  	@member = Member.find(params[:id])
+  	@member.destroy
+  	redirect_to :members, notice: "削除しました"
   end
 
   def search
-  	@members = Member.search(params[:q])
-  	render :index
-  end  
+    @members = Member.search(params[:q])
+    render :index
+  end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,9 +1,33 @@
 class Member < ActiveRecord::Base
-	def self.search(query)
-		rel = order(:number)
-		if query.present?
-			rel = rel.where("name LIKE ? OR full_name LIKE ?", "%#{query}", "%#{query}")
-		end 
-		rel
-	end 
+  include EmailAddressChecker
+
+  validates :number, presence: true,
+    numericality:{ only_integer: true, greter_than: 0, less_than: 100, allow_blank: true,
+                   uniqueness: true }
+
+    validates :name, presence: true,
+    format: { with:  /\A[A-Za-z]\w*\z/, allow_blank: true},
+    length: { minimum: 2, maximum: 20, allow_blank: true },
+    uniqueness: { case_sensitive: false }
+
+  validates :full_name, length: { maximum: 20 }
+
+  validate :check_email
+
+  def self.search(query)
+    rel = order(:number)
+    if query.present?
+      rel = rel.where("name LIKE ? OR full_name LIKE ?", "%#{query}", "%#{query}")
+    end
+    rel
+  end
+
+  private
+  def check_email
+    if email.present?
+      errors.add(:email, :invalid) unless well_formed_as_email_
+      address(email)
+    end
+  end
+
 end
